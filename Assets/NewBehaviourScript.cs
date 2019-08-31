e37c727ef12418a1c8f5078ad542857a550b1b39
Qarian <Ferno>
2019-08-31 16:53:14 +0200

    1

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+	[SerializeField]
+	Vector3 vel;
+
+	AudioSource source;
+	public AudioSource objecta;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.Translate(vel * Time.deltaTime);
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			source.loop = false;
+			source.Play();
+		}
+    }
+}
