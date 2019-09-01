082cc202d479b657d05bf2b999a2a16b12e0387f
Necr0o0 <radekklaser@gmail.com>
2019-09-01 02:05:57 +0200

    animations

---
+++ Assets/MoveLeftUIAnimationController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoveLeftUIAnimationController : MonoBehaviour
+{
+    private Animator _animator;
+
+    public Animator _animatorSecondMenu;
+    public Button nextMenu;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void ClickedButtonNextView()
+    {
+        _animator.SetTrigger("ButtonClicked");
+        _animatorSecondMenu.SetTrigger("ButtomClicked");
+    }
+}
