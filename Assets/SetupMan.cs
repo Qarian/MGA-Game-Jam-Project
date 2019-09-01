bdcb56fdc074f7185b3e3874dd7806c5d993dbd6
Necr0o0 <radekklaser@gmail.com>
2019-09-01 08:44:39 +0200

    zmiany jakies

--- Assets/SetupMan.cs
+++ Assets/SetupMan.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class SetupMan : MonoBehaviour
 {
+    public Skins skins;
+    
+    public GameObject head;
+    public GameObject spodnie;
+    public GameObject buty;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        head.GetComponent<MeshRenderer>().material = skins.head[skins.headID];
+        spodnie.GetComponent<MeshRenderer>().material = skins.body[skins.bodyID];
+        buty.GetComponent<MeshRenderer>().material = skins.legs[skins.legsID];
     }
     
 }
