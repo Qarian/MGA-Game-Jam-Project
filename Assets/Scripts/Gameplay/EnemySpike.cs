cf609ad4e6f34fcbaf0cf86e8b14ff219e34e8ae
Qarian <Ferno>
2019-09-01 08:24:09 +0200

    soundtrjhedf

--- Assets/Scripts/Gameplay/EnemySpike.cs
+++ Assets/Scripts/Gameplay/EnemySpike.cs
@@ -1,17 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class EnemySpike : MonoBehaviour
 {
-	private void OnCollisionEnter2D(Collision2D collision)
-	{
-		Debug.Log(collision);
-	}
-
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
-		Debug.Log(collision);
 		if (collision.tag == "Player")
 		{
 			PlayerMovement player = collision.GetComponent<PlayerMovement>();
---
+++ Assets/Scripts/Gameplay/UberJumpTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UberJumpTrigger : MonoBehaviour
+{
+	[SerializeField]
+	float newJumpForce = 200f;
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.tag == "Player")
+		{
+			PlayerMovement player = collision.GetComponent<PlayerMovement>();
+			player.jumpForce = newJumpForce;
+		}
+	}
+}
